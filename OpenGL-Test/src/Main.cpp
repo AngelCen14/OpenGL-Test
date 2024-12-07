b62b4dafef2b858f29126ec5d9739f926449e727
AngelCen14 <angelcen1447@gmail.com>
2024-12-07 14:58:41 +0100

    First commit - Libraries - Basic Window

---
+++ OpenGL-Test/src/Main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+int main() {
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(720, 720, "OpenGL Test", nullptr, nullptr);
+    if (!window) {
+        std::cout << "Failed to create GLFW window" << '\n';
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
